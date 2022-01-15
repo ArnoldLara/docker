e2a6287c55755e3d7931f2d3e75847b1e9c1e926
ArnoldLara <lara.arnoldandres@gmail.com>
2022-01-14 19:21:02 -0500

    Agregando volumenes con docker compose para reflejar los cambios del desarrollo

--- platzi/docker/index.js
+++ platzi/docker/index.js
@@ -12,9 +12,9 @@ const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
 app.get('/', (req, res) => {
   MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
     if (err) {
-      res.status(500).send('💥 BOOM 💥: ' + err);
+      res.status(500).send('💥 KABOOM 💥: ' + err);
     } else {
-      res.send('Me conecté a la DB! 😎');
+      res.send('Me conecté a la Base de Datos! 😎');
       db.close();
     }
   });
